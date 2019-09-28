eff4f4b1926362bd55ae4ac7fd31cb4f643ef54c
Sebastián Gurín <sgurin@awalabs.com>
2019-09-28 13:54:40 -0300

    version patch

--- docs/jsx-alone-dom-dom/sample-alone.js
+++ docs/jsx-alone-dom-dom/sample-alone.js
@@ -153,6 +153,13 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var event_1 = require("./event");
 var nodeHtml_1 = require("./util/nodeHtml");
@@ -235,7 +242,7 @@ var MNode = (function (_super) {
             nodes[_i] = arguments[_i];
         }
         var children = this._parentNode._children;
-        children.splice.apply(children, [children.indexOf(this), 1].concat(nodes.map(function (n) { return typeof n === 'string' ? _this.ownerDocument.createTextNode(n) : n; })));
+        children.splice.apply(children, __spreadArrays([children.indexOf(this), 1], nodes.map(function (n) { return typeof n === 'string' ? _this.ownerDocument.createTextNode(n) : n; })));
     };
     MNode.DOCUMENT_TYPE_NODE = 10;
     MNode.TEXT_NODE = 3;
